f5c3c7142b0e274a956bc96a2869532d876692ce
Geert Jan Bex <geertjan.bex@uhasselt.be>
2020-09-24 10:51:00 +0200

    Add plotting of binary files

--- source_code/io_performance/plot_spiral.sh
+++ source_code/io_performance/plot_spiral.sh
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
 
-gnuplot --persist << EOI
-    set size square
-    set polar
-    plot "$1" using 1:2 with dots
+if [ $# -ne 1 ]
+then
+    (>&2 echo "error: expecting data file as argument" )
+    exit 1
+fi
+
+if [ ! -e "$1" ]
+then
+    (>&2 echo "error: file '$1' does not exist" )
+    exit 2
+fi
+
+if [[ "$1" == *.txt ]]
+then
+    gnuplot --persist << EOI
+        set size square
+        set polar
+        plot "$1" using 1:2 with dots
 EOI
+elif [[ "$1" == *.bin ]]
+then
+    gnuplot --persist << EOI
+        set size square
+        set polar
+        plot "$1" binary format='%double%double' using 1:2 with dots
+EOI
+else
+    (>&2 echo "error: use files with extension .txt or .bin only" )
+    exit 3
+fi
