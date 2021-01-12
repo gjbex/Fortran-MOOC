b2206e06848b0204e42ca93f78ceb2c137c9f2e7
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-01-12 19:08:16 +0100

    Add source code for recursion scenario

---
+++ scenarios/Week_02/Recursion/fib_01.f90
@@ -0,0 +1,20 @@
+program fibonacci
+    implicit none
+
+    print *, fib(5)
+
+contains
+
+    function fib(n) result(res)
+        implicit none
+        integer, value :: n
+        integer :: res
+
+        if (n == 0 .and. n == 1) then
+            res = 1
+        else
+            res = fib(n - 1) + fib(n - 2)
+        end if
+    end function fib
+
+end program fibonacci
