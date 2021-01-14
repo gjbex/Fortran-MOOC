2df511201cf5c465dd7c5f406ce7e862122977f3
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-01-14 15:58:57 +0100

    Remove iostat and iomsg arguments

--- scenarios/Week_02/Files/write_file_01.f90
+++ scenarios/Week_02/Files/write_file_01.f90
@@ -2,13 +2,11 @@ program files
     implicit none
     integer, parameter :: nr_values = 10
     character(len=50), parameter :: filename = 'data.txt'
-    integer :: unit_nr, stat, i
-    character(len=1024) :: msg
+    integer :: unit_nr, i
     real :: x = 5.1
 
     open (newunit=unit_nr, file=filename, access='sequential', &
-          action='write', status='replace', form='formatted', &
-          iostat=stat, iomsg=msg)
+          action='write', status='replace', form='formatted')
     do i = 0, nr_values - 1
         write (unit=unit_nr, fmt='(F3.1)') i*x
     end do
