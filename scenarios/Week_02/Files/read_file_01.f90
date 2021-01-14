b609f9130ce599e7f5a98e5767ba306ab82f5e62
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-01-14 16:00:41 +0100

    Improve error handling

--- scenarios/Week_02/Files/read_file_01.f90
+++ scenarios/Week_02/Files/read_file_01.f90
@@ -2,7 +2,7 @@ program files
     use, intrinsic :: iso_fortran_env, only : error_unit, output_unit, &
                                               iostat_end
     implicit none
-    integer, parameter :: nr_values = 10
+    integer, parameter :: nr_values = 10, io_error = 1
     character(len=50), parameter :: filename = 'data.txt'
     integer :: unit_nr, stat
     character(len=1024) :: msg
@@ -13,7 +13,7 @@ program files
           iostat=stat, iomsg=msg)
     if (stat /= 0) then
         write (unit=error_unit,fmt='(A)') msg
-        stop 1
+        stop io_error
     end if
     do
         read (unit=unit_nr, fmt=*, iostat=stat, iomsg=msg) x
@@ -21,7 +21,7 @@ program files
             exit
         else if (stat > 0) then
             write (unit=error_unit,fmt='(A)') msg
-            stop 1
+            stop io_error
         end if
         write (unit=output_unit, fmt='(F8.1)') x**2
     end do
