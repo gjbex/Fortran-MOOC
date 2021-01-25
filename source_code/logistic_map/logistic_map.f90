46d57022fa0eceff1bbf9f72a8fb57c2bbb2c0f2
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-01-25 18:56:25 +0100

    Fix array index bug, add number of iterations parameter

--- source_code/logistic_map/logistic_map.f90
+++ source_code/logistic_map/logistic_map.f90
@@ -1,7 +1,7 @@
 program logistic_map
     use, intrinsic :: iso_fortran_env, only : DP => REAL64
     implicit none
-    integer, parameter :: nr_x_values = 100, nr_r_values = 100
+    integer, parameter :: nr_x_values = 100, nr_r_values = 200, nr_iterations = 10000
     real, parameter :: x_min = 0.0_DP, x_max = 1.0_DP, &
                        r_min = 0.5_DP, r_max = 4.0_DP
     real(kind=DP), dimension(nr_x_values, nr_r_values) :: X, R, result
@@ -23,7 +23,7 @@ program logistic_map
     end do
 
     ! compute result
-    result = iterate_map(X, R, 100)
+    result = iterate_map(X, R, nr_iterations)
     do i = 1, nr_x_values
         do j = 1, nr_r_values
             print *, R(i, j), result(i, j)
