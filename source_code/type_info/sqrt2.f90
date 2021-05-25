b494f9040b83e6e65573bb956dbc057476b8f4af
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-05-25 14:15:41 +0200

    Fix warning

--- source_code/type_info/sqrt2.f90
+++ source_code/type_info/sqrt2.f90
@@ -2,6 +2,6 @@ program sqrt_2
     use, intrinsic :: iso_fortran_env, only : SP => REAL32, DP => REAL64
     implicit none
 
-    print *, sqrt(2.0_SP) == 2.0_SP
-    print *, sqrt(2.0_DP) == 2.0_DP
+    print *, sqrt(2.0_SP) == 2.0_SP**2
+    print *, sqrt(2.0_DP) == 2.0_DP**2
 end program sqrt_2
