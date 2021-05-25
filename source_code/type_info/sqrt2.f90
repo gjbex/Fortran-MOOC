672b5e0a0632f192ace8910c2d5766aa714783bd
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-05-25 14:17:35 +0200

    Add approximate comparison

--- source_code/type_info/sqrt2.f90
+++ source_code/type_info/sqrt2.f90
@@ -1,7 +1,13 @@
 program sqrt_2
     use, intrinsic :: iso_fortran_env, only : SP => REAL32, DP => REAL64
     implicit none
+    real(kind=SP) :: sqr_root
+    real(kind=DP) :: sqr_root_dp
 
-    print *, sqrt(2.0_SP) == 2.0_SP**2
-    print *, sqrt(2.0_DP) == 2.0_DP**2
+    sqr_root = sqrt(2.0_SP)
+    sqr_root_dp = sqrt(2.0_DP)
+    print '(A, L)', 'sqrt(2)**2 == 2 (SP): ', sqr_root**2 == 2.0_SP
+    print '(A, L)', 'sqrt(2)**2 =~ 2 (SP): ', abs(sqr_root**2 - 2.0_SP) <= 1.0e-6_SP
+    print '(A, L)', 'sqrt(2)**2 == 2 (DP): ', sqr_root_dp**2 == 2.0_DP
+    print '(A, L)', 'sqrt(2)**2 =~ 2 (DP): ', abs(sqr_root_dp**2 - 2.0_DP) <= 1.0e-14_DP
 end program sqrt_2
