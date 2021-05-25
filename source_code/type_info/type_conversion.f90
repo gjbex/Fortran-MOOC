c2444c4c13dc41103138f267d4acc252ac66f901
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-05-25 14:17:06 +0200

    Fix formatting; fix conversion to int32

--- source_code/type_info/type_conversion.f90
+++ source_code/type_info/type_conversion.f90
@@ -5,14 +5,14 @@ program type_conversion
     implicit none
     real(kind=SP) :: x_sp
     real(kind=DP) :: x_dp
-    integer(kind=i32) :: i_i32
-    integer(kind=i64) :: i_i64
+    integer(kind=i32) :: i_I32
+    integer(kind=i64) :: i_I64
 
     x_dp = 1.0e100_DP
     x_sp = real(x_dp, kind=SP)
     print *, x_sp
 
     i_i64 = 2**40
-    i_i32 = int(i_i64, kind=I64)
-    print *, i_i32
+    i_I32 = int(i_I64, kind=I32)
+    print *, i_I32
 end program type_conversion
