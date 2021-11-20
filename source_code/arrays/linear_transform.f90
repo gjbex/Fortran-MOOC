8377ef7f5662e96554ef47c694edb804d5a3a59f
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-11-20 11:07:15 +0100

    Add example for elemental usage

--- source_code/arrays/linear_transform.f90
+++ source_code/arrays/linear_transform.f90
@@ -1,10 +1,15 @@
 program linear_transform
     implicit none
     real, dimension(5) :: values
+    real, dimension(2) :: a = [ 1.1, 2.2 ], &
+                          x = [ 0.5, 2.0 ], &
+                          b = [ -1.2, 2.5 ]
+    integer :: i
 
     call random_number(values)
     print *, values
     print *, lin_transform(values, 2.0, 1.0)
+    print *, lin_transform(x, a, b)
 
 contains
 
